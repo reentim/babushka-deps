846e9f2451bbb080518e92e26468744b2dcb0ccf
Timothy Reen <reentim@gmail.com>
2014-03-13 16:12:12 +1100

    Use babushka npm template, but don't install npm with brew.

--- osx/node.rb
+++ osx/node.rb
@@ -1,20 +1,29 @@
 dep 'node' do
     requires [
-      'node.managed',
+      'node-brew',
       'npm',
-      'bower.npm'
+      'node-packages',
     ]
 end
 
-dep 'node.managed'
-
-dep 'npm' do
-  requires 'node.managed'
-
-  met? { in_path? 'npm' }
+# NPM is broken under Homebrew: https://github.com/Homebrew/homebrew/issues/22408
+dep 'node-brew' do
+  met? { which 'node' }
   meet {
-    shell 'curl http://npmjs.org/install.sh | sh'
+    log_shell 'Installing node without npm', 'brew install node --without-npm'
   }
 end
 
+dep 'node-packages' do
+  requires %w[
+    coffee-script.npm
+    underscore.npm
+    bower.npm
+    grunt-cli.npm
+  ]
+end
+
+dep 'coffee-script.npm' do provides 'coffee' end
+dep 'underscore.npm'
 dep 'bower.npm'
+dep 'grunt-cli.npm'
--- osx/templates/npm.rb
+++
@@ -1,9 +0,0 @@
-meta :npm do
-  def npm
-    name.split('.').first
-  end
-  template {
-    met? { which npm }
-    meet { log_shell "Installing #{npm}", "npm install -g #{npm}" }
-  }
-end
