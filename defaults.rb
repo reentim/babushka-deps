14d0988adcf552bf2d1344e6a7bf379dcbc22b05
Timothy Reen <reentim@gmail.com>
2019-11-03 15:15:08 +1100

    Change default screenshot location to ~/cloud/Screenshots

--- defaults.rb
+++ defaults.rb
@@ -1,6 +1,7 @@
 dep 'preferences' do
   requires %w[
     dock-auto-hide.defaults
+    screenshot-location.defaults
     trackpad-tap-to-click.defaults
   ]
 end
@@ -8,6 +9,11 @@ end
 dep "dock-auto-hide.defaults" do
   write "com.apple.dock autohide -bool", true
   unwrite "com.apple.dock autohide -bool", false
+end
+
+dep "screenshot-location.defaults" do
+  write "com.apple.screencapture location", "~/iCloud-Drive/Screenshots".p
+  unwrite "com.apple.screencapture location", nil
 end
 
 dep "trackpad-tap-to-click.defaults" do
