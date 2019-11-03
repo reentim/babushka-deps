9494c2ec761d9873fd04962135a53da13dc2a3b8
Timothy Reen <reentim@gmail.com>
2019-11-03 18:35:47 +1100

    Add cloud.rb, for everything cloudy

--- cloud.rb
+++ cloud.rb
@@ -1,6 +1,16 @@
-dep 'cloud' do
-  requires 'symlink'.with(
+dep "cloud" do
+  requires "cloud-linked"
+  requires "cloud-log-dir"
+end
+
+dep "cloud-linked" do
+  requires "symlink".with(
     source: "~/Library/Mobile Documents/com~apple~CloudDocs".p,
-    target: "~/cloud",
+    target: "~/cloud".p,
   )
+end
+
+dep "cloud-log-dir" do
+  met? { "~/cloud/log".p.exists? }
+  meet { "~/cloud/log".p.mkdir }
 end
