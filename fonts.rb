e72182e843b6724df2f44db19edd7862901649be
Timothy Reen <reentim@gmail.com>
2019-11-18 13:38:02 +1100

    Don't install exotic weights for Recursive font

--- fonts.rb
+++ fonts.rb
@@ -5,22 +5,10 @@ end
 dep 'recursive.font' do
   source 'https://github.com/arrowtype/recursive/releases/download/v1.022/recursive-static_fonts-b020.zip'
   provides [
-    'Recursive Mono-Linear Black Italic.otf',
-    'Recursive Mono-Linear Black.otf',
     'Recursive Mono-Linear Bold Italic.otf',
     'Recursive Mono-Linear Bold.otf',
-    'Recursive Mono-Linear ExtraBold Italic.otf',
-    'Recursive Mono-Linear ExtraBold.otf',
     'Recursive Mono-Linear Italic.otf',
-    'Recursive Mono-Linear Light Italic.otf',
-    'Recursive Mono-Linear Light.otf',
-    'Recursive Mono-Linear Medium Italic.otf',
-    'Recursive Mono-Linear Medium.otf',
     'Recursive Mono-Linear Regular.otf',
-    'Recursive Mono-Linear SemiBold Italic.otf',
-    'Recursive Mono-Linear SemiBold.otf',
-    'Recursive Mono-Linear UltraBold Italic.otf',
-    'Recursive Mono-Linear UltraBold.otf',
   ]
 end
 
